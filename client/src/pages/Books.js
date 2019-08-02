08441d73be9ec987c82b445eb5ee6b4bdc49f46e
Isabella Wang <wangxbella0108@gmail.com>
2019-08-01 21:33:56 -0400

    update

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,61 +1,82 @@
 import React, { Component } from "react";
-import Jumbotron from "../components/Jumbotron";
-import DeleteBtn from "../components/DeleteBtn";
-import { Col, Row, Container } from "../components/Grid";
+import { Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Form";
+import { Input} from "../components/Form";
 import API from "../utils/API";
 
 class Books extends Component {
   // Initialize this.state.books as an empty array
   state = {
-    books: []
+    books: [],
+    search:"hi"
   };
 
   // Add code here to get all books from the database and save them to this.state.books
-  loadBook=()=>{
-    API.getBooks().then(res=> this.setState({books:res.data}))
+  loadBook = () => {
+    API.getBooks(this.state.search).then(res => {
+      this.setState({
+        books: res.data.items
+      });
+    }
+    )
   }
-  componentDidMount(){
+  searchBook=(event)=>{
+    this.setState({search:event.target.value});
     this.loadBook();
   }
+
+  componentDidMount() {
+    this.loadBook();
+  }
+
+  saveBook = event => {
+    event.preventDefault();
+      API.saveBook({
+        title: event.target.title,
+        author: event.target.author,
+        description: event.target.description,
+        href:event.target.href,
+        id:event.target.id
+      })
+        .then(res => {console.log(res)})
+        .catch(err => console.log(err));
+    // }
+  };
 
   render() {
     return (
       <Container fluid>
         <Row>
-          <Col size="md-6">
-            <Jumbotron>
+          {/* <Col size="md-6"> */}
+          {/* <Jumbotron>
               <h1>What Books Should I Read?</h1>
-            </Jumbotron>
-            <form>
-              <Input name="title" placeholder="Title (required)" />
-              <Input name="author" placeholder="Author (required)" />
-              <TextArea name="synopsis" placeholder="Synopsis (Optional)" />
-              <FormBtn>Submit Book</FormBtn>
+            </Jumbotron> */}
+          <form>
+              <Input name="title" placeholder="search by title" value={this.state.search} onChange={this.searchBook}/>
+              
             </form>
-          </Col>
-          <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>Books On My List</h1>
-            </Jumbotron>
-            {this.state.books.length ? (
-              <List>
-                {this.state.books.map(book => (
-                  <ListItem key={book._id}>
-                    <a href={"/books/" + book._id}>
-                      <strong>
-                        {book.title} by {book.author}
-                      </strong>
-                    </a>
-                    <DeleteBtn />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
+          {/* </Col> */}
+
+  
+        </Row>
+        <Row>
+          {this.state.books.length ? (
+            <List>
+              {this.state.books.map((book, i) => (
+                <ListItem key={i}>
+                  <a href={book.volumeInfo.previewLink}>
+                    {/* <img src={book.volumeInfo.imageLinks.previewLink} alt="hi"></img> */}
+                    <h3>title:{book.volumeInfo.title} </h3>
+                    <p> by {book.volumeInfo.authors}</p>
+                    <p>description:{book.volumeInfo.description}</p>
+                  </a>
+                  <button id={book.id} title={book.volumeInfo.title} href={book.volumeInfo.previewLin} description={book.volumeInfo.description} author={book.volumeInfo.authors} onClick={this.saveBook}>save</button>
+                </ListItem>
+              ))}
+            </List>
+          ) : (
               <h3>No Results to Display</h3>
             )}
-          </Col>
         </Row>
       </Container>
     );
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default {
   // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  getBooks: function(query) {
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q="+query+"+inauthor:keyes&key=AIzaSyD1iKCV3PUaH0lUBjnRZslCxz9TvZ6ori8");
   },
   // Gets the book with the given id
   getBook: function(id) {
